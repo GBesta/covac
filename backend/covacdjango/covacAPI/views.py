392f0fb1c01cc0f5e6f1ab7f4d4320555cdf05af
GBesta <besta98@gmail.com>
2021-11-30 22:32:44 +0100

    endpoint for create appointment

--- backend/covacdjango/covacAPI/views.py
+++ backend/covacdjango/covacAPI/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.response import Response 
+from rest_framework.response import Response
+
+from .models import Person 
 from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,6 +13,17 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+    def patch(self, request, *args, **kwargs):
+        token = Token.objects.get(key=request.data['token'])
+        user = User.objects.get(id= token.user_id)
+        user.person.appointmentDate = request.data["appointmentDate"][:10]
+        user.person.patientName = request.data["name"]
+        user.person.place = request.data["place"]
+        user.person.save(update_fields=['appointmentDate', 'patientName', 'place'])
+        return Response(status=204)
+
 
 class GetAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
